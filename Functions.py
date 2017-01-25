d13f83ae83e91a119be0acac6bfe45df4d996667
kennedygiles <kennedygiles250@gmail.com>
2017-01-25 11:23:29 -0800

    2nd half

--- Functions.py
+++ Functions.py
@@ -12,3 +12,8 @@ print(x)
 """2.  Write a program that will (1) ask the user for an input value, (2) take
 that and double it and  (3) print the result.
 Include necessary print statements and address whitespace issues."""
+
+input('s = ')
+s = s * i
+n = 2
+print(s)
